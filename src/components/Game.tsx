601a276b1ef9dc3d9219fae7256a3bf4dee29af9
Ana Oliveira <aol39@uclive.ac.nz>
2025-05-12 17:50:13 +1200

    add mui dialog for deleting a game

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,9 +1,84 @@
+import axios from "axios";
+import React from "react";
+import {Link, useNavigate, useParams } from "react-router-dom";
+
 // Commonly tsx file are placed in either a components or pages directory. With the distinction being
 // a page is often tied to a route and is composed of many components, with components being the tsx that
 // encapsulates smaller functionality.
 
+// useParams function allows us to access the id variable from our path
 const Game = () => {
-    return (<h1>Game</h1>)
+    const {id} = useParams();
+    const navigate = useNavigate();
+
+    // this is using the React useState hook to declare a piece of state called game with its initial value
+    // setGame is called later with the response data that then overrides the initialisation of game
+    // the initialisation ensure that the component can render safely before the API call completes
+    const [game, setGame] = React.useState<Game>({
+        creationDate: "",
+        creatorFirstName: "",
+        creatorId: 0,
+        creatorLastName: "",
+        genreId: 0,
+        price: 0,
+        rating: 0,
+        gameId:0, title:""})
+
+
+    const deleteGame = (game: Game) => {
+        axios.delete('http://localhost:4941/api/v1/games/' + id)
+            .then(response => {
+                navigate('/games')
+            }, (error) => {
+                setErrorFlag(true)
+                setErrorMessage(error.toString())
+        })
+    }
+
+    const [errorFlag, setErrorFlag] = React.useState(false)
+    const [errorMessage, setErrorMessage] = React.useState("")
+
+    React.useEffect(() => {
+        const getGame = () => {
+            axios.get('http://localhost:4941/api/v1/games/' + id)
+                .then((response) => {
+                    console.log("API response:", response.data);
+                    setErrorFlag(false)
+                    setErrorMessage("")
+                    setGame(response.data)
+                }, (error) => {
+                    setErrorFlag(true)
+                    setErrorMessage(error.toString())
+                })
+        }
+        getGame()
+    }, [id])
+
+    if (errorFlag) {
+        return (
+            <div>
+                <h1>Game</h1>
+                <div style={{ color: "pink" }}>
+                    {errorMessage}
+                </div>
+                <Link to={"/games/"}>Back to games</Link>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <h1>Game</h1>
+                {game.gameId}: {game.title}
+                <Link to={"/games/"}>Back to games</Link>
+                <button type={"button"}>
+                    Edit
+                </button>
+                <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#deleteGameModal">
+                    Delete
+                </button>
+            </div>
+        )
+    }
 }
 
 export default Game;
--- src/components/Games.tsx
+++ src/components/Games.tsx
@@ -1,21 +1,64 @@
 import axios from "axios";
-import {Link} from "react-router-dom";
 import React from "react";
+import {Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete"
+import {Link, useNavigate, useParams } from "react-router-dom";
+
 
 const Games = () => {
     const [games, setGames] = React.useState < Array < Game >> ([])
     const [errorFlag, setErrorFlag] = React.useState(false)
     const [errorMessage, setErrorMessage] = React.useState("")
 
+    const navigate = useNavigate();
+
     // React.useEffect -> fetches all games when the page
     React.useEffect(() => {
         getGames()
     }, [])
 
+    const [openDeleteDialog, setOpenDeleteDialog] = React.useState(false)
+    const [dialogGame, setDialogGame] = React.useState<Game>({
+        creationDate: "",
+        creatorFirstName: "",
+        creatorId: 0,
+        creatorLastName: "",
+        genreId: 0,
+        price: 0,
+        rating: 0,
+        gameId:0, title:""})
+
+    const handleDeleteDialogOpen = (game: Game) => {
+        setDialogGame(game)
+        setOpenDeleteDialog(true);
+    };
+    const handleDeleteDialogClose = () => {
+        setDialogGame({ creationDate: "",
+            creatorFirstName: "",
+            creatorId: 0,
+            creatorLastName: "",
+            genreId: 0,
+            price: 0,
+            rating: 0,
+            gameId:0, title:"" })
+        setOpenDeleteDialog(false);
+    };
+
+    const deleteGame = (game: Game) => {
+        axios.delete('http://localhost:4941/api/v1/games/' + game.gameId)
+            .then(response => {
+                navigate('/games')
+            }, (error) => {
+                setErrorFlag(true)
+                setErrorMessage(error.toString())
+            })
+    }
+
+
     const getGames = () => {
         axios.get('http://localhost:4941/api/v1/games')
             .then((response) => {
-                console.log("API response:", response.data);  // <- add this
+                console.log("API response:", response.data);
                 setErrorFlag(false)
                 setErrorMessage("")
                 setGames(response.data.games)
@@ -39,12 +82,15 @@ const Games = () => {
                 <td>{game.rating}</td>
                 <td><Link to={"/games/" + game.gameId}> Go to game </Link></td>
                 <td>
+                    <button type="button" onClick={() => handleDeleteDialogOpen(game)}>
+                        Delete
+                    </button>
                     <button type="button">Edit</button>
-                    <button type="button">Delete</button>
                 </td>
             </tr>
         )
     }
+
 
     if (errorFlag) {
         return (
@@ -79,7 +125,35 @@ const Games = () => {
                         {/*get all the table rows based on our games*/}
                         {list_of_games()}
                     </tbody>
-                </table>
+                    <Dialog
+                        open={openDeleteDialog}
+                        onClose={handleDeleteDialogClose}
+                        aria-labelledby="alert-dialog-title"
+                        aria-describedby="alert-dialog-description">
+                        <DialogTitle id="alert-dialog-title">
+                            {"Delete Game?"}
+                        </DialogTitle>
+                        <DialogContent>
+                            <DialogContentText id="alert-dialog-description">
+                                Are you sure you want to delete this game: <strong>{dialogGame.title}</strong>?
+                            </DialogContentText>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleDeleteDialogClose}>Cancel</Button>
+                            <Button
+                                variant="outlined"
+                                color="error"
+                                onClick={() => {
+                                    deleteGame(dialogGame)
+                                    handleDeleteDialogClose()
+                                    getGames()
+                                }}
+                                autoFocus>
+                                Delete
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
+            </table>
             </div>
         )
     }
